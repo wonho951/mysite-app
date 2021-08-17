d4c5d2d5ca25d260657a08925f7057630da28721
wonho <wonho951@gmail.com>
2021-08-17 10:17:50 +0900

    2021.08.17 Mysite

---
+++ app/src/main/java/com/javaex/mysite/MainActivity.java
@@ -0,0 +1,19 @@
+package com.javaex.mysite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //필드
+    //생성자
+    //메소드 g/s
+    //일반 메소드
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
