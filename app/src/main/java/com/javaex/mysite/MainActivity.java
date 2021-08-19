67c035f21ae6f5b773c16559a9df6ad33368e323
wonho <wonho951@gmail.com>
2021-08-19 11:01:53 +0900

    2021.08.19 툴바(홈버튼, 타이틀)

--- app/src/main/java/com/javaex/mysite/MainActivity.java
+++ app/src/main/java/com/javaex/mysite/MainActivity.java
@@ -1,8 +1,13 @@
 package com.javaex.mysite;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +20,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //툴바
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setTitle("방명록쓰기");
+    }
+
+    //item은 클릭한애를 알려줌
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {//뒤로가기 버튼 활성화
+
+        Log.d("javaStudy", "home버튼 클릭");
+        Log.d("javaStudy", "item.getItemId()-->"+item.getItemId());
+        Log.d("javaStudy", "android.R.id.home-->"+android.R.id.home);
+
+        //아이디를 알려줌
+        //홈버튼이 눌리면 종료해라
+        switch(item.getItemId()){
+            case android.R.id.home: //R.id.home아니라 안드로이드 붙여야함
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
     }
 }
